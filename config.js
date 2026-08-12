f70e562699363c310f84ec2175c1469fdf047f58
cerimonialbabr <cerimonial.babr@gmail.com>
2026-08-12 18:09:15 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.SGCM_CONFIG = {
   // Cole aqui a URL do Web App do Apps Script, terminada em /exec.
-  WEB_APP_URL: 'COLE_AQUI_A_URL_EXEC_DO_APPS_SCRIPT'
+  WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbzNsK5x4miQeOqZsVqgVL3vjiTjQvSbVSPptUHeu8qQiLUnTudqVLu91ABP7BQ7MYrS/exec'
 };
